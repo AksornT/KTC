0268b9536ad20445b1bb16fcffccdfe2115285ea
Win <onlyaksornt@gmail.com>
2018-08-01 15:57:51 +0700

    แก้ฟังก์ชั่นของ Global Header

--- ktc-web/js/win-ui.js
+++ ktc-web/js/win-ui.js
@@ -5,11 +5,7 @@ $(document).ready(function() {
 $(document).on('scroll', function() {
     if($(this).scrollTop()>=$('#head-trigger').position().top){
         $('#menu-global').addClass('mini-header');
-        $('.open-on-top').hide();
-    };
-
-    if($(this).scrollTop()<= 0){
+    }else{
     	$('#menu-global').removeClass('mini-header');
-    	$('.open-on-top').fadeIn();
     };
 });
