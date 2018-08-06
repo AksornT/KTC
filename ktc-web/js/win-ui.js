d97a1cc33bb45fc2f6f870f3295a2ae8767d5107
Win <onlyaksornt@gmail.com>
2018-08-06 15:29:26 +0700

    ปรับ Footer

--- ktc-web/js/win-ui.js
+++ ktc-web/js/win-ui.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-
+	$('.footer-menu h3').on('click touch', function () {
+        $('.footer-menu h3').not(this).each(function(){
+	        $(this).removeClass('active');
+	    });
+        $(this).toggleClass('active');
+    });
 
 });
+
+//change header when scroll down.
 $(document).on('scroll', function() {
     if($(this).scrollTop()>=$('#head-trigger').position().top){
         $('#menu-global').addClass('mini-header');
