2ea3384dd09f5a87d46e401951242bc42e8072c6
Suthida Sukkon <yves4869@gmail.com>
2018-08-07 12:32:01 +0700

    เมนูมือถือ

--- ktc-web/js/win-ui.js
+++ ktc-web/js/win-ui.js
@@ -16,3 +16,23 @@ $(document).on('scroll', function() {
     	$('#menu-global').removeClass('mini-header');
     };
 });
+
+//for menu mobile
+function myFunction() {
+    var x = document.getElementById("scrolled-menu-responsive");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+$(document).ready(function() {
+	$('.menu-responsive-dropdown h3').on('click touch', function () {
+        $('.menu-responsive-dropdown h3').not(this).each(function(){
+	        $(this).removeClass('active');
+	    });
+        $(this).toggleClass('active');
+    });
+
+});
