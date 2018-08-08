c5e1dd8452e2bbaa0349153ab76855de3eaf1cd7
Win <onlyaksornt@gmail.com>
2018-08-08 16:41:07 +0700

    pop-up search และ pop-up สมัครบัตร

--- ktc-web/js/win-ui.js
+++ ktc-web/js/win-ui.js
@@ -6,6 +6,29 @@ $(document).ready(function() {
         $(this).toggleClass('active');
     });
 
+    $(".to-top").on('click touch', function () {
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+    });
+
+
+    //search-category selected
+    $(".subcat-wrap label").on('click touch', function () {
+      $(this).parent().siblings().prop("checked", false);
+    });
+    $(".search-cat-wrap > label").on('click touch', function () {
+      $(".subcat-wrap input").prop("checked", false);
+    });
+
+
+    //profile
+    $('.footer-menu h3').on('click touch', function () {
+        $('.footer-menu h3').not(this).each(function(){
+            $(this).removeClass('active');
+        });
+        $(this).toggleClass('active');
+    });
+
     
 
 });
@@ -14,8 +37,17 @@ $(document).ready(function() {
 $(document).on('scroll', function() {
     if($(this).scrollTop()>=$('#head-trigger').position().top){
         $('#menu-global').addClass('mini-header');
+        $('.ghost-footer').addClass('active');
     }else{
-    	$('#menu-global').removeClass('mini-header');
+        $('#menu-global').removeClass('mini-header');
+        $('.ghost-footer').removeClass('active');
+    };
+
+//something appeared on footer
+    if($(this).scrollTop()>=$('#foot-trigger').position().top){
+        $('.ghost-footer').addClass('active-mobile');
+    }else{
+        $('.ghost-footer').removeClass('active-mobile');
     };
 });
 
