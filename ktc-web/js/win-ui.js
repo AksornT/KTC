0cc2dc52dabd7f0792b505842f60add7b50eed6f
Suthida Sukkon <yves4869@gmail.com>
2018-09-18 15:35:21 +0700

    edit font-size and follow comment

--- ktc-web/js/win-ui.js
+++ ktc-web/js/win-ui.js
@@ -605,7 +605,7 @@ function myFunction() {
 //for compare credit
 function myFunction() {
     var x = document.getElementById("myCompare");
-    if (x.style.display === "none") {
+    if (x.style.display == "none") {
         x.style.display = "block";
     } else {
         x.style.display = "none";
